9b1146141b8607ad143956c07c9188c245b2184a
Faynman <faynman@ms5.hinet.net>
2022-05-09 07:50:39 +0000

    add item

---
+++ demo2_arrow_function.js
@@ -0,0 +1,25 @@
+function displayCourseName(name) {
+    console.log('course name=${name}');
+}
+
+const displayCourseName2 = (name) => {
+    console.log('course name=${name}');
+}
+
+const displayCourseName3 = (name) => {
+    console.log('course name=${name}');
+}
+
+const displayCourseName4 = (name) => {
+    console.log('course name=${name}');
+}
+
+const displayCourseName5 = () => {
+    console.log('course name=${name}');
+}
+
+displayCourseName("VUE")
+displayCourseName2("React")
+displayCourseName3("Angular")
+displayCourseName4("JS")
+displayCourseName5()

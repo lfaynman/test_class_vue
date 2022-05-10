b5ed74794faff9c890c1f94f50d3e87e0ff36c7b
Faynman <faynman@ms5.hinet.net>
2022-05-10 01:48:19 +0000

    vue2_lab

--- LAB1_PLAIN_JS/app.js
+++ LAB1_PLAIN_JS/app.js
@@ -1,11 +1,8 @@
-const i1 = document.querySelector('input')
-const l1 = document.querySelector('ul')
-function addPlan(...args: []) {
-    const value = i1.value
-    const item = document.createElement('li')
-    item.textContent = value
-    l1.appendChild(item)
-    i1.value = ''
-}
-const b1 = document.querySelector('button')
-b1.addEventListener('click', addPlan)
+Vue.createApp({
+    data() { return { plans: [], value: '' } },
+    methods: {
+        addPlan() {
+            this.plans.push(this.value)
+        }
+    }
+}).mount('#myapp')

454a630cb82ad3307d8277f7e6c2d8f9eb94939e
Faynman <faynman@ms5.hinet.net>
2022-05-10 03:40:48 +0000

    test

--- LAB4/app.js
+++ LAB4/app.js
@@ -1,10 +1,26 @@
 const app = Vue.createApp({
     data:function(){
         return {
-            primaryGoal: "Have a happy life!",
+            primaryGoal: "<em>創新</em>",
+            primaryGoal2: "<em>客戶導向</em>",
+            primaryGoal3: "<em>當責</em>",
             planLink: "https://www.trello.com"
+        }
+    },
+    methods:{
+        outputMessage: function () {
+            const number1 = Math.random();
+            if (number1 < 0.3) {
+                return this.primaryGoal;
+            } else if (number1 < 0.6) {
+                return this.primaryGoal2;
+            } else {
+                return this.primaryGoal3;
+            }
+ 
         }
 
     }
+
 })
 app.mount("#my-planner")
---
+++ LAB5/app.js
@@ -0,0 +1,9 @@
+const app =Vue.createApp({
+    data() {
+        return{
+            counter1:0
+        }
+    },
+    methods:{}
+})
+app.mount("#app1")

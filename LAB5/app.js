a14f35ee7f9cc3d340ed1cc35a313d79ea6c7c13
Faynman <faynman@ms5.hinet.net>
2022-05-10 06:01:42 +0000

    using prevant modify

--- LAB5/app.js
+++ LAB5/app.js
@@ -1,9 +1,33 @@
-const app =Vue.createApp({
+const app = Vue.createApp({
     data() {
-        return{
-            counter1:0
+        return {
+            counter1: 0,
+            todo: ''
         }
     },
-    methods:{}
+    methods: {
+        submitForm(ev) {
+            ev.preventDefault()
+            alert('已經提交了~~~')
+        },
+        submitForm2() {
+            alert('還是提交了')
+        },
+        setToDo(event, message) {
+            this.todo = `${message} ${event.target.value}`
+        },
+        increase1() {
+            this.counter1 += 1;
+        },
+        decrease1() {
+            this.counter1 -= 1;
+        },
+        increase2(step) {
+            this.counter1 += step;
+        },
+        decrease2(step) {
+            this.counter1 -= step;
+        }
+    }
 })
 app.mount("#app1")

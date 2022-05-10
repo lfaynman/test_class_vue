b681b44bedd6b1abba0d6889b886dba525f02fa8
Faynman <faynman@ms5.hinet.net>
2022-05-10 06:47:46 +0000

    createLab6

--- LAB5/app.js
+++ LAB5/app.js
@@ -2,16 +2,29 @@ const app = Vue.createApp({
     data() {
         return {
             counter1: 0,
-            todo: ''
+            todo: '',
+            urgent: '',
+            committedUrgent: '',
+            counter2: 888
         }
     },
     methods: {
+        increase2_4(step) {
+            this.counter2 += step
+        },
+        decrease2_4(step) { this.counter2 -= step },
+        setUrgent(ev) {
+            this.urgent = ev.target.value
+        },
+        commitUrgent() {
+            this.committedUrgent = this.urgent
+        },
         submitForm(ev) {
             ev.preventDefault()
             alert('已經提交了~~~')
         },
         submitForm2() {
-            alert('還是提交了')
+            alert('還是提交了321')
         },
         setToDo(event, message) {
             this.todo = `${message} ${event.target.value}`

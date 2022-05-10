025d677163da45c57aaac8b57b753a00ec5cd3b8
Faynman <faynman@ms5.hinet.net>
2022-05-10 02:34:21 +0000

    LAB4 link

--- LAB4/app.js
+++ LAB4/app.js
@@ -1,6 +1,10 @@
 const app = Vue.createApp({
     data:function(){
-        return {primaryGoal: "Have a happy life!"}
+        return {
+            primaryGoal: "Have a happy life!",
+            planLink: "https://www.trello.com"
+        }
+
     }
 })
 app.mount("#my-planner")

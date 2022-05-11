472358e6aac113f76d1bd4ef3e20f2548cc71428
Faynman <faynman@ms5.hinet.net>
2022-05-11 06:28:55 +0000

    up LAB9

--- LAB9/app.js
+++ LAB9/app.js
@@ -13,5 +13,29 @@ const app = Vue.createApp({
         }
     }
 })
+app.component("course-content", {
+    template: `
+    <li>
+    <h2>{{c.id}}</h2>
+    <button @click="toggleCourseDetail">show details</button>
+    <ul v-if="detailsVisible">
+        <li>{{c.name}}</li>
+        <li>{{c.duration}}</li>
+    </ul>
+    </li>
 
+    `,
+    data() {
+        return {
+            detailsVisible: true,
+            c: { id: "POOP", name: "Python OOP", duration: 35 }
+        }
+    },
+    methods: {
+        toggleCourseDetail() {
+            this.detailsVisible = !this.detailsVisible;
+        }
+    }
+
+})
 app.mount('#app')

92c156559d15ad634112f25bea42b88483d76a5b
Faynman <faynman@ms5.hinet.net>
2022-05-11 02:16:19 +0000

    update LAB7

--- LAB7/app.js
+++ LAB7/app.js
@@ -10,12 +10,23 @@ const app = Vue.createApp({
         divSelected(id) {
             console.log(`id=${id}被選取了!`)
             if (id === 1) {
-                this.div1Selected = true
+                this.div1Selected = !this.div1Selected
             } else if (id === 2) {
-                this.div2Selected = true
+                this.div2Selected = !this.div2Selected
             } else if (id === 3) {
-                this.div3Selected = true
+                this.div3Selected = !this.div3Selected
             }
+        }
+    },
+    computed:{
+        div1Classes(){
+            return {is1Selected:this.div1Selected}
+        },
+        div2Classes(){
+            return {is1Selected:this.div1Selected}
+        },
+        div3Classes(){
+            return {is1Selected:this.div1Selected}
         }
     }
 })
